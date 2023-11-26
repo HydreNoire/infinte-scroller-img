710e1d2e063000fe0a15b1c48bfff2414b9e7e7f
Lise <lise692@gmail.com>
2023-11-26 21:42:19 +0100

    add some style on the listitem

--- src/components/ImageListItem/ImageListItem.jsx
+++ src/components/ImageListItem/ImageListItem.jsx
@@ -1,5 +1,21 @@
 import s from "./style.module.css";
 
 export function ImageListItem({ img }) {
-  return <img src={img.download_url} className={s.img} />;
+  return (
+    <div className={s.card}>
+      <a href={img.url}>
+        <img src={img.download_url} className={s.img} alt={img.author} />
+      </a>
+      <div className={s.card_banner}>
+        <img src={img.download_url} alt="The author" className={s.card_thumb} />
+        <div className={s.card_text}>
+          <h3 className={s.card_title}>{img.author}</h3>
+          <div className={s.card_status}>
+            Originale size: {img.height}x{img.width}
+          </div>
+          <button className={s.card_btn}>Download</button>
+        </div>
+      </div>
+    </div>
+  );
 }
